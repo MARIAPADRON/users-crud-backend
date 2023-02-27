afae10ab7669b17bf70282edfaa61481a9b6e0f1
unknown <mariaapadron7@gmail.comgit config --global user.name Mariagit config user.email mariaapadron7@gmail.com>
2023-02-27 18:25:45 -0300

    first commit

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,48 @@
+const catchError = require('../utils/catchError');
+const User = require('../models/User');
+
+const getAll = catchError(async(req, res) => {
+    const users = await User.findAll();
+    return res.json(users);
+});
+
+const create = catchError(async(req, res) => {
+    const {first_name, last_name, email, password, birthday} = req.body;
+    const user = await User.create({first_name, last_name, email, password, birthday});
+    return res.status(201).json(user);
+});
+
+const getOne = catchError(async(req, res) => {
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+    if (!user) return res.status(404).json({message: "User not found"})
+    return res.json(user);
+});
+
+const remove = catchError(async(req, res) => {
+    const { id } = req.params;
+    const userDelet = await User.destroy({where: {id}});
+    if (userDelet === 0) return res.status(404).json({message: "User not found"})
+    return res.sendStatus(204);
+});
+
+const update = catchError(async(req, res) => {
+    const {first_name, last_name, email, password, birthday} = req.body;
+    const { id } = req.params;
+    const user = await User.update(
+        {first_name, last_name, email, password, birthday},
+        {where: {id} , returning: true}
+    )
+    if(user [0]=== 0) return res.status(404).json({message: "User not found"})
+    return res.json(user[1][0]);
+});
+
+
+module.exports = {
+    getAll,
+    create,
+    getOne,
+    remove,
+    update
+    
+}
